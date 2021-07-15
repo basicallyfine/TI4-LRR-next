85e0b3eb311e28137c895841a8f064fc81ed54d7
Nic <nic@askable.com>
2021-07-15 12:55:42 +1000

    hacky dom replacement

--- pages/glossary.js
+++ pages/glossary.js
@@ -1,62 +1,102 @@
-import { Fragment } from 'react';
+import { useLayoutEffect, useState, Fragment } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 import Page from '../components/page';
 
 import * as glossary from '../content/glossary';
 
+function ItemNumber({ size = "text-sm", number })  {
+    return (
+        <span className={`item-number-wrapper relative width-0 overflow-visible`} >
+            <a href={`#${number}`} className={`${size} absolute -left-9 bottom-0`}>
+                {number}
+            </a>
+        </span>
+    );
+}
+
+function itemNumberMd({ size = "text-sm", number }) {
+    return `<span class="relative width-0 overflow-visible"><a href="#${number}" class="${size} absolute -left-9 bottom-0">${number}</a></span> `;
+}
+
 export default function Glossary() {
-    const content = [
-        <ReactMarkdown key="preamble" children={glossary.preamble} />
-    ];
+
+    const [val, setVal] = useState(true);
+
+    useLayoutEffect(() => {
+        const content = window.document.getElementById('glossary-content');
+        const items = content.getElementsByClassName('glossary-item');
+        
+        for (let i = 0; i < items.length; i += 1) {
+            const firstLink = items[i].getElementsByTagName('a').item(0);
+            if (firstLink.hasAttribute('class')) return;
+            
+            const span = window.document.querySelector('.item-number-wrapper').cloneNode();
+            firstLink.setAttribute('class', 'text-sm absolute -left-9 bottom-0');
+            firstLink.parentNode.prepend(span);
+            span.appendChild(firstLink);
+        }
+    })
+
+    const content = [];
     content.push(glossary.content.map((section, sectionIdx) => {
         const sectionNumber = sectionIdx + 1;
         let itemNumber = 1;
         const sectionItems = [];
 
+        if (section.name) {
+            sectionItems.push((
+                <h2 key={section.name}>
+                    <ItemNumber number={sectionNumber} size="text-base" />
+                    {section.name}
+                </h2>
+            ));
+        }
+
         section.subsections.forEach((subsection) => {
             if (subsection.name) {
                 sectionItems.push(
-                    <Fragment key={`subsection-${sectionNumber}-${itemNumber}`}>
-                        <dt className="subsection-name">{sectionNumber}.{itemNumber}</dt>
-                        <dd className="subsection-name">
-                            <h3 key={subsection.name}>{subsection.name}</h3>
-                            {subsection.preamble && <ReactMarkdown children={subsection.preamble} />}
-                        </dd>
-                    </Fragment>
+                    <h3 key={`subsection-${sectionNumber}-${itemNumber}`}>
+                        <ItemNumber number={`${sectionNumber}.${itemNumber}`} />
+                        {subsection.name}
+                    </h3>
                 );
+                if (subsection.preamble) {
+                    sectionItems.push((
+                        <div key={`preamble-${sectionNumber}-${itemNumber}`}>
+                            <ReactMarkdown
+                                key={`subsection-${sectionNumber}-${itemNumber}`}
+                                children={subsection.preamble}
+                            />
+                        </div>
+                    ));
+                }
                 itemNumber += 1;
             }
             subsection.items.forEach((item) => {
+                const number = `${sectionNumber}.${itemNumber}`;
                 sectionItems.push(
-                    <Fragment key={`item-${sectionNumber}-${itemNumber}`}>
-                        <dt className="subsection-name">{sectionNumber}.{itemNumber}</dt>
-                        <dd className="subsection-name"><ReactMarkdown children={item} /></dd>
-                    </Fragment>
+                    <div className="glossary-item" key={`item-${sectionNumber}-${itemNumber}`}>
+                        <ReactMarkdown children={`[${number}](#${number}) ${item}`} />
+                    </div>
                 );
                 itemNumber += 1;
             })
         });
         
-        return (
-            <Fragment>
-                <dl className="glossary-section">
-                    <dt className="section-name">{sectionIdx + 1}</dt>
-                    <dd className="section-name">
-                        <h2 key={section.name}>{section.name}</h2>
-                        {section.preamble && <ReactMarkdown children={section.preamble} />}
-                    </dd>
-                </dl>
-                <dl className="glossary-subsections">
-                    {sectionItems}
-                </dl>
-            </Fragment>
-        )
+        return sectionItems;
     }));
+
     return (
         <Page
             title="Glossary"
-            content={content}
+            content={(
+                <Fragment>
+                    <ReactMarkdown key="preamble" children={glossary.preamble} />
+                    <button onClick={() => { setVal(!val); }}>{val ? 'TRUE' : 'FALSE'}</button>
+                    <div className="pl-9 leading-6" id="glossary-content">{content}</div>
+                </Fragment>
+            )}
         />
     );
 }
